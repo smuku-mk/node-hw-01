5fd19fe833f4a644053f33983364eead1d564bd5
Mateusz Kubiak <smuku.mk@gmail.com>
2023-11-02 14:29:06 +0100

    step_02 begin

---
+++ contacts.js
@@ -0,0 +1,21 @@
+/*
+ * Skomentuj i zapisz wartość
+ * const contactsPath = ;
+ */
+
+// TODO: udokumentuj każdą funkcję
+const listContacts = () => {
+  // ...twój kod
+};
+
+const getContactById = (contactId) => {
+  // ...twój kod
+};
+
+const removeContact = (contactId) => {
+  // ...twój kod
+};
+
+const addContact = (name, email, phone) => {
+  // ...twój kod
+};
